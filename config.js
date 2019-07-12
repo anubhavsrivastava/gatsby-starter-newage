4c8e09179a2703e51741d5510e46ff70065caa50
Anubhav Srivastava <anubhav.srivastava00@gmail.com>
2019-07-13 00:14:20 +0530

    added social icons

--- config.js
+++ config.js
@@ -8,4 +8,27 @@ module.exports = {
   manifestDisplay: 'standalone',
   manifestIcon: 'src/assets/img/website-icon.png',
   pathPrefix: `/gatsby-starter-newage/`, // This path is subpath of your hosting https://domain/portfolio
+  // social
+  socialLinks: [
+    {
+      icon: 'fa-github',
+      name: 'Github',
+      url: 'https://github.com/anubhavsrivastava',
+    },
+    {
+      icon: 'fa-linkedin-in',
+      name: 'Linkedin',
+      url: 'https://linkedin.com/in/theanubhav/',
+    },
+    {
+      icon: 'fa-twitter',
+      name: 'Twitter',
+      url: 'https://twitter.com/onlyanubhav',
+    },
+    {
+      icon: 'fa-facebook-f',
+      name: 'Facebook',
+      url: 'https://facebook.com/theanubhav',
+    },
+  ],
 };
