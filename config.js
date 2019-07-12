3ed05b1d6107cf6d2477452d574235bf928dc784
Anubhav Srivastava <anubhav.srivastava00@gmail.com>
2019-07-12 23:02:22 +0530

    added site config

---
+++ config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  siteTitle: 'Gatsby Starter New age', // <title>
+  manifestName: 'newage',
+  manifestShortName: 'Landing', // max 12 characters
+  manifestStartUrl: '/',
+  manifestBackgroundColor: '#663399',
+  manifestThemeColor: '#663399',
+  manifestDisplay: 'standalone',
+  manifestIcon: 'src/assets/img/website-icon.png',
+  pathPrefix: `/gatsby-starter-newage/`, // This path is subpath of your hosting https://domain/portfolio
+};
